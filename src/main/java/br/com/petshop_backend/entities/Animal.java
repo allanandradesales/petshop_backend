b4af1e03b06737ee75fae9df8c4e1c36b8d05300
Allan Sales <allanandradesales@gmail.com>
2024-04-09 20:20:12 -0300

    first commit

---
+++ src/main/java/br/com/petshop_backend/entities/Animal.java
@@ -0,0 +1,8 @@
+package br.com.petshop_backend.entities;
+
+public class Animal {
+    private Long id;
+    private String nome;
+    private String raca;
+    private Float peso;
+}
---
+++ src/test/java/br/com/petshop_backend/PetshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petshop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
