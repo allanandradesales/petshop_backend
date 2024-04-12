3e81aafe0d933d9262bf27647f068d37840856c9
Allan Sales <allanandradesales@gmail.com>
2024-04-12 09:08:49 -0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	db/Petshop.mv.db

--- src/main/java/br/com/petshop_backend/repositories/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.petshop_backend.repositories;
-
-import br.com.petshop_backend.entities.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-}
